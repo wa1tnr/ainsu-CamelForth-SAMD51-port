04e6d2c3abdcdabfe6c17500cd9232f0966e68f6
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-03 16:25:46 +0000

    push()/pop() oriented variant routine On branch camel_forth_e_00a-fa- 	modified:   itoacf.c

--- src/cf/itoacf.c
+++ src/cf/itoacf.c
@@ -11,3 +11,16 @@ void itoacf(void) { // int to ascii, with write (printf substitute)
     io_write(io, (uint8_t *)pr_strn, len_pr_strn);
     _spc(); // print formatting/spacing
 }
+
+
+void itoacfp(void) { // int to ascii, with write (printf substitute)
+    int len_pr_strn = 0;
+    int popped = pop();
+    // int popped = *psp++ ;
+    pr_strn[0] = (uint32_t) "\0";
+    input_intgr = popped;
+    itoa (input_intgr, pr_strn);
+    len_pr_strn = strlen(pr_strn);
+    io_write(io, (uint8_t *)pr_strn, len_pr_strn);
+    _spc(); // print formatting/spacing
+}
