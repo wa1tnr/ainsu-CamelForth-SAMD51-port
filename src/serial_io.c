fb0d3150c5c596797f1d3f2114b3f0deda4d2f8b
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-03 05:19:13 +0000

    Branch stamped.
    On branch camel_forth_e_00a-
    
    	modified:   serial_io.c

--- src/serial_io.c
+++ src/serial_io.c
@@ -1,4 +1,4 @@
-// Mon Sep  3 05:13:23 UTC 2018
+// Mon Sep  3 05:17:29 UTC 2018
 // On branch camel_forth_e_00a-
 
 /* wa1tnr - September, 2018 LGPL v2.1 */
@@ -25,12 +25,11 @@
 #include "dict_common.h"
 
 #define timeStamp(t,l) \
-    "Mon Sep  3 05:13:23 UTC 2018\r\n\r\n", 32
+    "Mon Sep  3 05:17:29 UTC 2018\r\n\r\n", 32
 
-/*
+// current target branch:
 #define branchStamp(b,l) \
     "On branch camel_forth_e_00a-   ", 31
-*/
 
 // "zKM_converser_d51-aa- +CamelForth +asm   ", 41
 // 1234567 101234567 201234567 301234567 4012345678901234567890
@@ -39,9 +38,9 @@
 #define branchStamp(b,l) "KM_converser_d51         ", 25
 */
 
-// current target branch:
+/*
 #define branchStamp(b,l) "master    +CamelForth    ", 25
-
+*/
 
 struct io_descriptor *io;
 
