23f11688537d724cec954570e61e3c26bf8dd69d
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 07:44:03 +0000

    did: #define stack_buffer_length 64
    On branch camel_forth_to_stand_alone
    	modified:   ainsu_common.h

--- src/cf/ainsu_common.h
+++ src/cf/ainsu_common.h
@@ -3,5 +3,6 @@
 #include "atmel_start.h"
 #define ainsu_maxtib 16
 // the stack needs a buffer for ASCII strings - the dot word has this
-#define stackbufferlen 64
+// #define stackbufferlen 64
+#define stack_buffer_length 64
 extern uint8_t *ainsu_buf;
