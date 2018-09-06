0ad3f94c548472f2e0d1e8ddc53e88a6cce9ec4d
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 07:53:23 +0000

    < extern char pr_strn[stackbufferlen];
     > extern char print_string[stack_buffer_length];
    
    On branch camel_forth_to_stand_alone
    
    	modified:   cmf_itoa.h

--- src/cf/cmf_itoa.h
+++ src/cf/cmf_itoa.h
@@ -1,7 +1,12 @@
-extern char pr_strn[stackbufferlen];
+extern char print_string[stack_buffer_length];
+
 extern int input_integer;
-extern void call_me_now_ms_cleo(void);
+
+// extern void call_me_now_ms_cleo(void);
+
 extern void cmf_itoa(int n, char s[]);
-extern uint8_t *buf;
+
+// extern uint8_t *buf;
+
 #include "usart_gateway.h"
 #include <string.h>
