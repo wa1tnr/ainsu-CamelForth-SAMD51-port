4bcca499ed73ebcde066531772cf508a2d8954de
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 03:56:14 +0000

    Near finalization.  Timestamped and branch-stamped.
    On branch camel_forth_to_stand_alone
    	modified:   main.c

--- main.c
+++ main.c
@@ -1,20 +1,10 @@
-// Wed Aug 29 04:49:35 UTC 2018
-// On branch xKM_converser_d51-nn-
+// Thu Sep  6 03:55:27 UTC 2018
+// On branch camel_forth_to_stand_alone
 
 #include <atmel_start.h>
-#include "pins.h"
-#include "clock_gen_OE.h"
+// #include "pins.h"
+// #include "clock_gen_OE.h"
 #include "usart_gateway.h"
-/*
-#include "dump.h"
-#include "nyb_stack.h"
-#include "common.h"
-#include "itoa.h"
-#include "stack_ops.h"
-#include "forth.h"
-*/
-
-/* */ /* */ /* */ /* */
 
 int main(void)
 {
@@ -22,11 +12,10 @@ int main(void)
     atmel_start_init();
     SystemInit();
     // 8 MHz CPU
-    pins_setup(); // initialize GPIO D13 PA17
+    // pins_setup(); // initialize GPIO D13 PA17
 
-    clock_gen_oe(); // clock generator 0 output enable
+    // clock_gen_oe(); // clock generator 0 output enable
 
-    // USART_0_example_upper(); // initialize
     USART_0_example_upper_camelforth(); // atmel start USART driver
 
     /* Replace with your application code */
