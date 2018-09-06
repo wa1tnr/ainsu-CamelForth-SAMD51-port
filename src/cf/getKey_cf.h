f428123f0802f6ec6f918e063a103c26c0b0919f
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 05:58:25 +0000

    New getKey variant - getKey_cf.h - the include file On branch camel_forth_to_stand_alone 	new file:   getKey_cf.h

---
+++ src/cf/getKey_cf.h
@@ -0,0 +1,11 @@
+// Wed Aug 22 18:00:54 UTC 2018
+/* wa1tnr - August, 2018 LGPL v2.1 */
+
+// On branch KM_converser_aa-
+//     On branch xKM_converser_aa-br-bb-
+
+extern char ch[];
+extern int read_ln;
+extern char tib[maxtib]; /* Terminal Input Buffer for interpreter */
+extern void getKey(void); // hardware-independent wrapper
+extern void queryKey(void);
