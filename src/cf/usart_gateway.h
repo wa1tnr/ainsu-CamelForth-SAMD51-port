523140e8e0fed2ab516c1ab9d5decee720b5b07c
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 04:26:20 +0000

    add extern void USART_0_example_lower_camelforth(void);
    On branch camel_forth_to_stand_alone
    
    	modified:   usart_gateway.h

--- src/cf/usart_gateway.h
+++ src/cf/usart_gateway.h
@@ -1,10 +1,3 @@
-// #ifndef DRIVER_EXAMPLES_H_INCLUDED
-// #define DRIVER_EXAMPLES_H_INCLUDED
-
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-
 extern struct io_descriptor *io;
 
 extern void fg_white();
@@ -20,10 +13,4 @@ extern void _spc(void);
 extern void _cr(void); // line 113
 extern void cr(void);
 extern void USART_0_example_upper_camelforth(void);
-// extern void USART_0_example_upper(void);
-extern void USART_0_example_lower(void);
-
-// #ifdef __cplusplus
-// }
-// #endif
-// #endif // DRIVER_EXAMPLES_H_INCLUDED
+extern void USART_0_example_lower_camelforth(void);
