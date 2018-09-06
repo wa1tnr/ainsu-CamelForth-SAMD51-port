72b630d229c72b3aecf80f905d7bab6d9d2bac40
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 06:20:31 +0000

    File common.h effectively no longer in the program. May need to reinstate some of it, elsewhere. On branch camel_forth_to_stand_alone 	modified:   ../common.h

--- common.h
+++ common.h
@@ -1,7 +1,7 @@
 /* common.h */
 // types
-#include "atmel_start.h"
-#define maxtib 16
+// #include "atmel_start.h"
+// #define maxtib 16
 // the stack needs a buffer for ASCII strings - the dot word has this
-#define stackbuflen 64
-extern uint8_t *buf;
+// #define stackbuflen 64
+// extern uint8_t *buf;
