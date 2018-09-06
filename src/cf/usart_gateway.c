ce87e59654def2a981e3464d6760b0d0d6e2ff72
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 07:55:41 +0000

    Hmm.  camelforth() is called here (and nowhere else).
    Tail wagging the dog?
    
     > #include "getKey_cf.h" // SAMD51 dependency for now (06 SEP 2018)
    
    On branch camel_forth_to_stand_alone
    
    	modified:   usart_gateway.c

--- src/cf/usart_gateway.c
+++ src/cf/usart_gateway.c
@@ -3,19 +3,9 @@
 
 /* wa1tnr - September, 2018 LGPL v2.1 */
 
-// Tue Sep  4 09:08:22 UTC 2018
-// On branch camel_forth_to_stand_alone
-
-// Mon Sep  3 16:42:24 UTC 2018
-// On branch camel_forth_e_00a-fa-
-
-// Wed Aug 29 05:03:11 UTC 2018
-// On branch xKM_converser_d51-nn-
-
-// #include "serial_io.h"
 #include "driver_init.h"
-#include "common.h"
-#include "getKey.h"
+// #include "common.h"
+#include "getKey_cf.h" // SAMD51 dependency for now (06 SEP 2018)
 #include "utils.h"
 #include "warm.h"
 #include "readword.h"
@@ -27,15 +17,11 @@
 extern void camelforth(void); // pivotal - do not omit!
 
 #define timeStamp(t,l) \
-    "Thu Sep  6 03:55:27 UTC 2018\r\n\r\n", 32
+    "Thu Sep  6 03:55:27 UTC 2018\n\n", 32
 
 // current target branch:
 #define branchStamp(b,l) \
     "On branch camel_forth_to_stand_alone    ", 40
-/*
-#define branchStamp(b,l) \
-    "On branch camel_forth_e_00a-fa-    ", 35
-*/
 
 /*
 #define branchStamp(b,l) "master    +CamelForth    ", 25
@@ -85,6 +71,12 @@ void fg_white(void) {
     io_write(io, (uint8_t *)"m", 1);        // for the stanza
 }
 
+void fg_magenta(void) {
+    io_write(io, (uint8_t *)"\033\133", 2); // ESC [
+    io_write(io, (uint8_t *)"\063\065", 2); // 35 - yellow fg
+    io_write(io, (uint8_t *)"m", 1);        // for the stanza
+}
+
 void fg_yellow(void) { // foreground yellow
     io_write(io, (uint8_t *)"\033\133", 2); // ESC [
     io_write(io, (uint8_t *)"\063\063", 2); // 33 - yellow fg
@@ -103,18 +95,16 @@ void _que(void) {
 }
 
 void _ok(void) {
-    if ( DEBUG_FORTH_DICT_PRIMITIVES ) {
-        io_write(io, (uint8_t *) " ~serial_io.c LINE 73: ",  23);
-    }
     if (crlfstate == -1) {
         io_write(io, (uint8_t *) " ok\r\n", 5); // echo TODO \r\n on some terminals
         crlfstate = 0;
     }
 }
 
+#define HAS_FILTER_AINSU
+#undef HAS_FILTER_AINSU
+#ifdef HAS_FILTER_AINSU
 void filter(void) {
-
-    // capture_warm();
 
     /* stanza -- control H */
     if ((uint8_t) *buf == 8) { // Control H
@@ -138,6 +128,7 @@ void filter(void) {
         return;
     }
 }
+#endif // #ifdef HAS_FILTER_AINSU
 
 void _bkbar(void) {
     io_write(io, (uint8_t *) "|",         1);
@@ -170,69 +161,41 @@ void dispatcher_simple_int(void) {
 void USART_0_example_lower_camelforth(void) {
     _cr();
 
-    camelforth(); // right here is where CamelForth is invoked!
-
-#define HAS_HELLO_INTERPRETER
-#undef HAS_HELLO_INTERPRETER
-#ifndef HAS_HELLO_INTERPRETER
-
-    io_write(io, (uint8_t *) "USART_0_example_upper() .. completes.\r\n", 39); // is alive
-#endif // #ifndef HAS_HELLO_INTERPRETER
-
-#ifdef HAS_HELLO_INTERPRETER
-
     io_write(io, (uint8_t *)
-        "Program is configured for 38400 bps speed.\r\n\r\n",        46);
+        "Program is configured for 38400 bps speed.\n",              43);
     io_write(io, (uint8_t *)
-        "Target MCU board is Adafruit Metro M4 Express.\r\n\r\n",    50);
+        "Target MCU board is Adafruit Metro M4 Express.\n",          47);
 
     color_reset();
-
     io_write(io, (uint8_t *)
-        "UART pins TX and RX used with CP2104 bridge.\r\n\r\n",      48);
+        "UART pins TX and RX used with CP2104 bridge.\n",            45);
 
     io_write(io, (uint8_t *) branchStamp(t,l));
-
-/*
-    io_write(io, (uint8_t *)
-        "xKM_converser_d51-jj-atmel_start_verif-    ", 43); // private branch - see KM_converser_d51 instead
-*/
-    // PRESERVE BRANCH_STAMP for lineage verification:
-    //  "xKM_converser_d51-gg-    ", 25); // private branch - see KM_converser_d51 instead
-
-    //  12345678901234567890123456789012345678901234567890
-    // "KM_converser_d51         ", 25);
-
-    // prep for merge into master, 25 Aug 20:58 UTC:
-    // "On branch master         ", 25); // reflect that this is really branch master
-
-/*
-    io_write(io, (uint8_t *)
-        "SMUDGE BIT SET   ", 17); // SMUDGE (means nothing other than 'this is different'
-*/
-
     io_write(io, (uint8_t *) timeStamp(t,l));
 
-    // PRESERVE TIMESTAMP for lineage verification:
-    //  "Mon Aug 27 04:04:40 UTC 2018\r\n\r\n",                      32);
 
     bg_blue();
     fg_white();
     bg_red();
     io_write(io, (uint8_t *)  "  ainsuForth MTX  ",  18);
     color_reset();
+    io_write(io, (uint8_t *)  "\n\n",                 2);
+    color_reset();
 
     bg_black();
- // io_write(io, (uint8_t *)"    type 'words': \r\n\r\n",  24);
-    io_write(io, (uint8_t *)"                  \r\n\r\n",  22);
     bg_black();
 
     fg_yellow(); // color it!
 
-    while(-1) { // endless loop, read one char, write one char (echo)
-        readword();
-        runword();
-    }
 
-#endif // #ifdef HAS_HELLO_INTERPRETER
+    while(-1) { // endless loop
+        camelforth(); // right here is where CamelForth is invoked!
+        io_write(io, (uint8_t *) "\n", 1);
+        fg_magenta();
+        io_write(io, (uint8_t *) "USART_0_example_upper_camelforth() .. completes.\n", 49); // is alive
+        color_reset();
+        io_write(io, (uint8_t *) "\n", 1);
+     // readword(); // ainsu hooks
+     // runword();  // ainsu hooks
+    }
 }
