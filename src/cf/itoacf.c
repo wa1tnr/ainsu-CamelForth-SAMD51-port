5782acf14dee6d7e2aeb6bead6a2dec0e9751fc3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-06 07:50:13 +0000

    renamed.
     < void itoacf(void) {     // int to ascii, with write (printf substitute)
    
     > void itoa_write(void) { // int to ascii, with write (printf substitute)
    
    On branch camel_forth_to_stand_alone
    	modified:   itoacf.c

--- src/cf/itoacf.c
+++ src/cf/itoacf.c
@@ -1,18 +1,28 @@
 #include "itoacf.h"
 
-void itoacf(void) { // int to ascii, with write (printf substitute)
-    int len_pr_strn = 0;
-    // int popped = pop();
-    int popped = *psp++ ;
-    pr_strn[0] = (uint32_t) "\0";
-    input_integer = popped;
-    cmf_itoa(input_integer, pr_strn);
-    len_pr_strn = strlen(pr_strn);
-    io_write(io, (uint8_t *)pr_strn, len_pr_strn);
+void itoa_write(void) { // int to ascii, with write (printf substitute)
+
+    int len_pr_strn = 0;   // DECLARE LENGTH VARIABLE
+    int popped             // DECLARE DATA INPUT VARIABLE
+                = *psp++ ; // AND POPULATE IT FROM THE STACK
+
+//  pr_strn[0] = (uint32_t) "\0"; // ZERO INIT GLOBAL STRING
+
+//  extern char print_string[stack_buffer_length];
+
+    print_string[0] = (uint32_t) "\0"; // ZERO INIT GLOBAL STRING
+
+    input_integer = popped; // COPY DATA INTO GLOBAL
+    cmf_itoa(input_integer, print_string); // SCAN
+
+    len_pr_strn = strlen(print_string); // LENGTH OF OUTPUT
+    io_write(io, (uint8_t *)print_string, len_pr_strn); // WRITE OUTPUT TO USART
     _spc(); // print formatting/spacing
+
 }
 
 
+/*
 void itoacfp(void) { // int to ascii, with write (printf substitute)
     int len_pr_strn = 0;
     // int popped = pop();
@@ -24,3 +34,4 @@ void itoacfp(void) { // int to ascii, with write (printf substitute)
     io_write(io, (uint8_t *)pr_strn, len_pr_strn);
     _spc(); // print formatting/spacing
 }
+*/
