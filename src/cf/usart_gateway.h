67310c5c6f35b4e4d42abd8de912d04e9c238973
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-07 00:27:14 +0000

    More ANSI color stuff On branch progressive_Edits_aa 	modified:   usart_gateway.h

--- src/cf/usart_gateway.h
+++ src/cf/usart_gateway.h
@@ -1,11 +1,17 @@
 extern struct io_descriptor *io;
 
-extern void fg_white();
-extern void fg_yellow();
-extern void bg_black();
-extern void bg_blue();
-extern void bg_red();
-extern void color_reset();
+extern void bold_italics_white(void);
+extern void italics(void);
+extern void bold_italics(void);
+extern void n1uro_italics(void);
+extern void fg_white_force_bold(void);
+extern void fg_white(void);
+extern void fg_yellow(void);
+extern void bg_black(void);
+extern void bg_blue(void);
+extern void bg_red(void);
+extern void color_reset(void);
+extern void color_bold(void); // experimental
 extern void _que(void);
 extern void _ok(void);
 extern void _bkbar(void); // broken bar
