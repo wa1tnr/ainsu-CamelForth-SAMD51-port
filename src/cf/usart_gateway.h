dd0786ef205f6f5fba2ad37de65e5b6636b17ded
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-07 04:10:05 +0000

    On branch progressive_Edits_ad- 	modified:   usart_gateway.h

--- src/cf/usart_gateway.h
+++ src/cf/usart_gateway.h
@@ -6,18 +6,17 @@ extern struct io_descriptor *io;
 #define timeStamp(t,l) \
     "Fri Sep  7 04:05:50 UTC 2018\n\n", 32
 
-/*
+// current target branch:
 #define branchStamp(b,l) \
     "On branch progressive_Edits_ad-    ", 35
-*/
 
 /*
     "On branch camel_forth_to_stand_alone    ", 40
 */
 
-// current target branch:
+/*
 #define branchStamp(b,l) "master                   ", 25
-
+*/
 
 extern void bold_italics_white(void);
 extern void italics(void);
